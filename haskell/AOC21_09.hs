2d04a5589fc23febdfe154ee37827af3468aa1d6
Alexion Ramos <ratex98@gmail.com>
2022-01-11 17:30:58 -0500

    AOC21 Day 9 Problem 2
    It's kinda ugly though

--- haskell/AOC21_09.hs
+++ haskell/AOC21_09.hs
@@ -4,28 +4,27 @@ module AOC21_09 where
 import Data.Char (digitToInt)
 import Data.Foldable (Foldable (foldl'))
 import Data.Function ((&))
-import Data.List (transpose)
+import Data.List (sort, transpose)
 
 ---------------
 -- Problem 1 --
 ---------------
 p1 :: [String] -> IO ()
 p1 input =
-  lows xs
-    & zipWith (zipWith (\n b -> if b then n + 1 else 0)) xs
-    & concat
-    & sum
-    & print
-  where
-    xs = parse input
+  let xs = parse input
+   in lows xs
+        & zipWith (zipWith (\n b -> if b then n + 1 else 0)) xs
+        & concat
+        & sum
+        & print
 
 parse :: [String] -> [[Int]]
 parse = fmap (fmap digitToInt)
 
 lows :: [[Int]] -> [[Bool]]
-lows xs = zipWith (zipWith (&&)) (fmap low xs) (transpose $ fmap low xs')
-  where
-    xs' = transpose xs
+lows xs =
+  let xs' = transpose xs
+   in zipWith (zipWith (&&)) (fmap low xs) (transpose $ fmap low xs')
 
 low :: [Int] -> [Bool]
 low [x1, x2, x3] = [x1 < x2, x2 < x1 && x2 < x3, x3 < x2]
@@ -36,4 +35,52 @@ low _ = error "bad input"
 -- Problem 2 --
 ---------------
 p2 :: [String] -> IO ()
-p2 = print
+p2 input =
+  let xs = parse input
+   in basins xs
+        & sort
+        & reverse
+        & take 3
+        & foldl' (*) 1
+        & print
+
+basins :: [[Int]] -> [Int]
+basins xs =
+  concatMap
+    (filter (/= 0) . fmap snd)
+    ( fmap (fmap codify) xs
+        & evalRows
+        & evalCols
+        & evalRows
+        & evalCols
+        & evalRows
+        & evalCols
+    )
+
+codify :: Int -> (Int, Int)
+codify 9 = (9, 0)
+codify x = (x, 1)
+
+evalRows :: [[(Int, Int)]] -> [[(Int, Int)]]
+evalRows = fmap (eval' . eval)
+
+evalCols :: [[(Int, Int)]] -> [[(Int, Int)]]
+evalCols xs = transpose $ evalRows (transpose xs)
+
+eval :: [(Int, Int)] -> [(Int, Int)]
+eval [x] = [x]
+eval ((h, c) : (h', c') : xs) =
+  if h' < h
+    then (h, 0) : eval ((h', c + c') : xs)
+    else (h, c) : eval ((h', c') : xs)
+eval _ = error "bad input"
+
+eval' :: [(Int, Int)] -> [(Int, Int)]
+eval' [x] = [x]
+eval' ((h, c) : xs) =
+  let xs' = eval' xs
+      (xh, xc) = head xs'
+   in if h < xh
+        then (h, c + xc) : (xh, 0) : tail xs'
+        else (h, c) : xs'
+eval' _ = error "bad input"
