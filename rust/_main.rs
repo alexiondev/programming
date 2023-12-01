b9e0b7d01d3088fcae5855bf08fb0d71a13fbc48
Alexion Ramos <alexiondev@gmail.com>
2023-12-01 05:06:33 -0500

    AOC23 Day 1 Problem 1 & 2

--- rust/_main.rs
+++ rust/_main.rs
@@ -2,12 +2,17 @@ mod aoc23_01;
 
 use std::env;
 
-fn main() {
+use anyhow;
+
+fn main() -> anyhow::Result<()>{
     let raw_args: Vec<String> = env::args().collect();
     let args: Vec<&str> = raw_args.iter().map(AsRef::as_ref).collect();
 
     match args[1..] {
-        ["aoc23", "1", "1"] => aoc23_01::problem1(),
+        ["aoc23", "1", "1"] => aoc23_01::problem1()?,
+        ["aoc23", "1", "2"] => aoc23_01::problem2()?,
         _ => println!("Unknown problem!")
     }
+
+    Ok(())
 }
--- rust/aoc23_01.rs
+++ rust/aoc23_01.rs
@@ -1,3 +1,92 @@
-pub fn problem1() {
-    println!("Hello world!");
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+};
+use regex::Regex;
+
+use anyhow::{self, bail};
+
+const INPUT_PATH : &str = "_input/aoc23/01";
+
+fn input() -> Vec<String> {
+    let file = File::open(INPUT_PATH).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+fn find_first_digit(iter: impl Iterator<Item = char>) -> anyhow::Result<u32> { 
+    for c in iter {
+        if let Some(x) = c.to_digit(10) {
+            return Ok(x);
+        }
+    }
+
+    bail!("Did not find a digit");
+}
+
+pub fn problem1() -> anyhow::Result<()> {
+    let mut numbers: Vec<i32> = Vec::new();
+    for line in input() {
+        let first_digit = find_first_digit(line.chars())?;
+        let last_digit = find_first_digit(line.chars().rev())?;
+
+        let x = format!("{}{}", first_digit, last_digit).parse::<i32>().unwrap();
+        numbers.push(x);
+    }
+
+    let sum: i32 = numbers.iter().sum();
+    println!("{}", sum);
+    Ok(())
+}
+
+fn find_first_digit_word(iter: impl Iterator<Item = char>) -> anyhow::Result<u32> {
+    let mut buf = String::new();
+
+    for c in iter {
+        if let Some(x) = c.to_digit(10) {
+            return Ok(x);
+        } else {
+            buf.push(c);
+        }
+
+        if let Some(x) = parse_number(&buf) {
+            return Ok(x);
+        }
+    }
+
+    bail!("Did not find a digit");
+}
+
+fn parse_number(s: &str) -> Option<u32> {
+    match s {
+        "zero" | "orez" => Some(0),
+        "one" | "eno" => Some(1),
+        "two" | "owt" => Some(2),
+        "three" | "eerht" => Some(3),
+        "four" | "ruof" => Some(4),
+        "five" | "evif" => Some(5),
+        "six" | "xis" => Some(6),
+        "seven" | "neves" => Some(7),
+        "eight" | "thgie" => Some(8),
+        "nine" | "enin" => Some(9),
+        "" => None,
+        _ => parse_number(&s[1..s.len()]),
+    }
+}
+
+pub fn problem2() -> anyhow::Result<()> {
+    let mut numbers: Vec<i32> = Vec::new();
+    for line in input() {
+        let first_digit = find_first_digit_word(line.chars())?;
+        let last_digit = find_first_digit_word(line.chars().rev())?;
+
+        let x = format!("{}{}", first_digit, last_digit).parse::<i32>().unwrap();
+        numbers.push(x);
+    }
+
+    let sum: i32 = numbers.iter().sum();
+    println!("{}", sum);
+    Ok(())
 }
